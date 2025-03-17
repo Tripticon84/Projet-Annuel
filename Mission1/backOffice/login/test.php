3615df02d25a87263366db4ca068b0e43418ee4d
Tripticon84 <yannjou@free.fr>
2025-03-17 14:46:00 +0100

    suppresion du fichier test.php

--- Mission1/backOffice/login/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-include_once $_SERVER['DOCUMENT_ROOT'] . "/api/dao/admin.php";
-include_once $_SERVER['DOCUMENT_ROOT'] . "/api/utils/server.php";
-
-makeApiRequest("api/admin/login.php", ["yann@bc.com", "r"]);
