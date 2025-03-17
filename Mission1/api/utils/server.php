df305795ee3871baadd4b71acef7103d1d05c3c3
Tripticon84 <yannjou@free.fr>
2025-03-17 20:40:37 +0100

    Refactor token verification to support multiple user roles and improve error handling

--- Mission1/api/utils/server.php
+++ Mission1/api/utils/server.php
@@ -50,26 +50,108 @@ function validateMandatoryParams(array $data, array $mandatoryParams): bool
     return true;
 }
 
-function tokenVerification($token)
-{
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/admin.php';
 
+function acceptedTokens($admin = true, $company = false, $employee = false, $provider = false) {
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/admin.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/employee.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/company.php';
+    include_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/provider.php';
 
-    if (!isset($token)) {
+    $debug = false;
+    if ($debug) {
+        return;
+    }
+
+    $headers = getallheaders();
+    if (isset($headers['Authorization'])) {
+        $token = str_replace('Bearer ', '', $headers['Authorization']);
+    } elseif (isset($headers['authorization'])) {
+        $token = str_replace('Bearer ', '', $headers['authorization']);
+    } else {
         returnError(401, 'Unauthorized: Missing token');
         return;
     }
-    $adminToken = getAdminByToken($token);
-    if (!$adminToken) {
-        returnError(401, 'Unauthorized: Invalid token');
-        return;
-    }
-    $expirationDate = getExpirationByToken($token);
-    if ($expirationDate) {
-        $expiration = new DateTime($expirationDate['expiration']);
-        if ($expiration < new DateTime()) {
-            returnError(401, 'Unauthorized: Token expired');
+
+    $adminUser = getAdminByToken($token);
+    if ($adminUser) {
+        if (!$admin) {
+            returnError(403, 'Unauthorized: Admin access not allowed');
             return;
         }
+        $expirationDate = getExpirationByToken($token);
+        if ($expirationDate) {
+            $expiration = new DateTime($expirationDate['expiration']);
+            if ($expiration < new DateTime()) {
+                returnError(401, 'Unauthorized: Token expired');
+                return;
+            }
+        } else {
+            returnError(500, 'Internal Server Error: Unable to retrieve expiration date');
+            return;
+        }
+        return; // Admin is authorized
     }
+
+    $employeeUser = getEmployeeByToken($token);
+    if ($employeeUser) {
+        if (!$employee) {
+            returnError(403, 'Unauthorized: Employee access not allowed');
+            return;
+        }
+        $expirationDate = getEmployeeExpirationByToken($token);
+        if ($expirationDate) {
+            $expiration = new DateTime($expirationDate['expiration']);
+            if ($expiration < new DateTime()) {
+                returnError(401, 'Unauthorized: Token expired');
+                return;
+            }
+        } else {
+            returnError(500, 'Internal Server Error: Unable to retrieve expiration date');
+            return;
+        }
+        return; // Employee is authorized
+    }
+
+    $companyUser = getCompanyByToken($token);
+    if ($companyUser) {
+        if (!$company) {
+            returnError(403, 'Unauthorized: Company access not allowed');
+            return;
+        }
+        $expirationDate = getCompanyExpirationByToken($token);
+        if ($expirationDate) {
+            $expiration = new DateTime($expirationDate['expiration']);
+            if ($expiration < new DateTime()) {
+                returnError(401, 'Unauthorized: Token expired');
+                return;
+            }
+        } else {
+            returnError(500, 'Internal Server Error: Unable to retrieve expiration date');
+            return;
+        }
+        return; // Company is authorized
+    }
+
+    $providerUser = getProviderByToken($token);
+    if ($providerUser) {
+        if (!$provider) {
+            returnError(403, 'Unauthorized: Provider access not allowed');
+            return;
+        }
+        $expirationDate = getProviderExpirationByToken($token);
+        if ($expirationDate) {
+            $expiration = new DateTime($expirationDate['expiration']);
+            if ($expiration < new DateTime()) {
+                returnError(401, 'Unauthorized: Token expired');
+                return;
+            }
+        } else {
+            returnError(500, 'Internal Server Error: Unable to retrieve expiration date');
+            return;
+        }
+        return;
+    }
+
+    // Si on arrive ici, le jeton n'est valide pour aucun utilisateurs
+    returnError(401, 'Unauthorized: Invalid token');
 }
