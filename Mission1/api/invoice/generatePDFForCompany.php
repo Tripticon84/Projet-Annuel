134fad7821dd54450fd4b947f4f81c98eb37b480
Makun <mathieubrt1609@gmail.com>
2025-04-03 18:29:22 +0200

    jcrois ca marche normalement pour les pdfs reste plus qu'a le mettre dans le front hihihihi UwU

---
+++ Mission1/api/invoice/generatePDFForCompany.php
@@ -0,0 +1,41 @@
+<?php
+
+
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/dao/invoice.php';
+require_once $_SERVER['DOCUMENT_ROOT'] . '/api/utils/server.php';
+
+header('Content-Type: application/json');
+
+if (!methodIsAllowed('read')) {
+    returnError(405, 'Method not allowed');
+    return;
+}
+
+acceptedTokens(true, true, false, false);
+
+$data = getBody();
+
+
+$factureId = $data['facture_id'];
+
+if (empty($factureId)) {
+    returnError(400, 'Mandatory parameter : factureId');
+}
+
+if (!is_numeric($factureId)) {
+    returnError(400, 'factureId must be a number');
+}
+
+if ($factureId < 0) {
+    returnError(400, 'factureId must be a positive number');
+}
+
+if ($factureId) {
+    $pdf= generatePDFForCompany($factureId);
+    
+    http_response_code(200);
+    echo json_encode($pdf);
+    
+}else {
+    returnError(500, 'No invoice found');
+}
