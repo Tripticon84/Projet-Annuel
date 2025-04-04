991bff93e10e1dedc0c79bbb18e7697b3cdf3f22
Tripticon84 <yannjou@free.fr>
2025-04-04 16:03:21 +0200

    fix logout

--- Mission1/backOffice/login/logout.php
+++ Mission1/backOffice/login/logout.php
@@ -5,8 +5,8 @@ session_unset();
 session_destroy();
 
 // Suprimer le cookie token
-if (isset($_COOKIE['token'])) {
-    setcookie('token', '', time() - 3600, '/');
+if (isset($_COOKIE['admin_token'])) {
+    setcookie('admin_token', '', time() - 3600, '/');
 }
 
 header("Location: ../index.php");
